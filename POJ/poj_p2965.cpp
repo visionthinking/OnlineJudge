261d61bcdc93286c2ba9cffbd868fbaef0d0c248
RainVision <rainvision2012@gmail.com>
2015-03-30 11:57:27 +0800

    Signed-off-by: RainVision <rainvision2012@gmail.com>

--- POJ/poj_p2965.cpp
+++ POJ/poj_p2965.cpp
@@ -3,9 +3,6 @@
 
 #define MAX 65536
 
-//speed up: op(), consider remove hash/unhash, and use bit operator instead
-int FLIP[16] = {4383, 8751, 17487, 34959, 4593, 8946, 17652, 35064, 7953, 12066, 20292, 36744, 61713, 61986, 62532, 63624};
-
 inline int hash(char * b){
 	int h = 0;
 	for(int i=0;i<16;i++){
@@ -135,14 +132,6 @@ void solution(){
 int main(void){
 	freopen("p2965.in", "r", stdin);
 	
-	/*
-	for(int i=0;i<16;i++){
-		char a[16];
-		memset(a, 0, sizeof(a));
-		op(a, i);
-		printf("%d,\n", hash(a));
-	}
-	*/
 	solution();
 	return 0;
 }
