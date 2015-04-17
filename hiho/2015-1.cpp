ad457531c20217d1d4cf79b1bd45177d1356118f
RainVision <rainvision2012@gmail.com>
2015-04-17 16:44:14 +0800

    2015

--- hiho/2015-1.cpp
+++ hiho/2015-1.cpp
@@ -2,11 +2,8 @@
 #include <string.h>
 
 const int MAX 	= 2000000000;
-const int MAX_S =     100001;
 const char* MONTH[12] = {"January", "February", "March", "April", "May", "June", 
 	"July", "August", "September", "October", "November" , "December"};
-
-int s[MAX_S];
 
 inline
 bool isRunYear(int year){
@@ -23,44 +20,6 @@ int getMonth(char * a){
 	return -1;
 }
 
-int getSum(int y1, int y2){
-	int count = 0;
-	int i;
-	i = y1;
-	if(y2 - y1 >= 10000){
-		count = (y2 - y1)/10000 * 2425;
-		i = y1 + ((y2 - y1)/10000)*10000;
-	}
-	for(;i<=y2;i++){
-		if(i % 4 == 0){
-			break;
-		}
-	}	
-	for(;i<=y2;i+=4){
-		if(isRunYear(i)){
-			count += 1;
-		}
-	}
-	return count;
-}
-
-
-void debug(){
-	int i, t;
-	s[0] = getSum(1992000, 2000000);
-	printf("\ncalc:\n");
-	printf("s[0]=%d", s[0]);
-	for(i=1;i<=MAX_S;i++){
-		//s[i] = getSum(20000*i, 20000*(i+1)-1);
-		int diff = 1237;
-		t = getSum(10000*i + diff, 10000*(i+1)-1 + diff);
-		if(t != 2425){
-			printf("%d\n", t);
-		}
-	}
-}
-
-
 void solution(){
 	int y1, m1, d1, y2, m2, d2;
 	int i, j, k, count;
@@ -70,27 +29,29 @@ void solution(){
 	scanf("%s %d, %d", str, &d2, &y2);
 	m2 = getMonth(str);
 	
-	count = getSum(y1+1, y2-1);
 	
-	if(y1 < y2){
+	count = 0;
+	if(y1 == y2){
 		if(isRunYear(y1)){
-			if(m1 <= 1 || (m1 == 2 && d1 <= 29)){
-				count += 1;
-			}
-		}
-		if(isRunYear(y2)){
-			if(m2 > 2 || (m2 == 2 && d2 == 29)){
-				count += 1;
+			if(m1<=2 && (m2>2 || (m2==2 && d2>=29))){
+				count = 1;
 			}
 		}
 	}else{
+		count = (y2/4 - y2/100 + y2/400) - (y1/4 - y1/100 + y1/400);
 		if(isRunYear(y1)){
-			if((m1 <= 1 || (m1 == 2 && d1 <= 29))
-			&& (m2 > 2 || (m2 == 2 && d2 == 29))){
-				count += 1;	
+			count += 1;
+			if(!(m1<=2)){
+				count -= 1;
+			}
+		}
+		if(isRunYear(y2)){
+			if(!(m2>2 || (m2==2 && d2>=29))){
+				count -= 1;
 			}
 		}
 	}
+	
 	printf("%d", count);
 }
 
