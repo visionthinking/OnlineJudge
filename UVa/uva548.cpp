b1421609f96ce613fabc21973da68b8849307794
RainVision <rainvision2012@gmail.com>
2015-05-13 14:21:36 +0800

    Accepted, 112ms.
    可以采用迭代代替递归加深深度优化。
    但是数据较弱，直接AC，在此就略过了。

--- UVa/uva548.cpp
+++ UVa/uva548.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 struct node {
 	int data;
-	int left, right;	
+	int left, right;
 };
 const int MAX = 20000;
 struct node tree[MAX];
@@ -16,19 +16,38 @@ int tlen;
 stringstream stream;
 int a[MAX];
 int b[MAX];
+long long minsum;
+long long minval;
 
+inline 
 int alloc(){
+	tree[tlen].data = -1;
 	tree[tlen].left = tree[tlen].right = -1;
 	return tlen++;	
 }
 
-void build(int n, int al, int bl, int len){
+void debug(int n){
+	if(n == -1) return;
+	
+	printf("debug: %d: %d\n", n, tree[n].data);
+	debug(tree[n].left);
+	debug(tree[n].right);
+}
+
+void build(int n, int al, int bl, int len, long long sum){
 	if(len <= 0) return;
 	int x, i;
-	if(len == 1){
-		tree[n].data = a[al];
-	}
 	x = b[bl+len-1];
+	tree[n].data = x;
+	if(len == 1){
+		if(minsum > sum + x){
+			minsum = sum + x;
+			minval = x;
+		}else if(minsum == sum + x && minval > x){
+			minval = x;
+		}
+		return;
+	}
 	for(i=0;i<len;i++){
 		if(a[al+i] == x){
 			break;	
@@ -40,9 +59,11 @@ void build(int n, int al, int bl, int len){
 	rlen = len - llen - 1;
 	if(llen > 0){
 		tree[n].left = alloc();
+		build(tree[n].left, al, bl, llen, sum + x);
 	}
 	if(rlen > 0){
 		tree[n].right = alloc();
+		build(tree[n].right, al+llen+1, bl+llen, rlen, sum + x);
 	}
 }
 
@@ -68,11 +89,16 @@ void solve(const string & inorder, const string & postorder){
 	
 	tlen = 0;
 	alloc();
-	//build(0, 0, len-1);
+	minsum = 1000000;
+	minval = 1000000;
+	build(0, 0, 0, len, 0);
+	
+	printf("%lld\n", minval);
 }
 
 int main(void){
 	freopen("uva548.in", "r", stdin);
+	ios::sync_with_stdio(false);
 	string a, b;
 	while(getline(cin, a)){
 		getline(cin, b);
