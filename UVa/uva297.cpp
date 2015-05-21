0bbf937ea2b1624c4626cdd4f3accac5f7accabe
RainVision <rainvision2012@gmail.com>
2015-05-22 00:31:40 +0800

    update

--- UVa/uva297.cpp
+++ UVa/uva297.cpp
@@ -4,44 +4,56 @@
 #include <iostream>
 using namespace std;
 
-string a, b;
-int pa, pb;
+struct node {
+	char c;
+	int child[4];
+};
+
+const int MAX = 10000;
+struct node tree1[MAX];
+struct node tree2[MAX];
+int len;
+
+int alloc(struct node * tree){
+	memset(tree[len].child, -1, sizeof(int)*4);
+	return len++;
+}
 
 //'p': grey, 'f': black, 'e': white
-int build(int area){
-	if(pa >= a.length() || pb >= b.length()){
-		return 0;	
+int p;
+void build(struct node * tree, const string & str){
+	if(p >= str.length()){
+		return;
 	}
-	char ca = a[pa];
-	char cb = b[pb];
-	int result = 0;
-	if(ca == 'f' || cb == 'f'){
-		if(ca == 'p'){
-			//遍历每遇到一个p要pa+=4	
-		}else{
-			pa += 1;	
-		}
-		if(cb == 'p'){
-				
-		}else{
-			pb += 1;	
-		}
-		return area;
+	int k;
+	char c = str[p];
+	p += 1;
+	
+	if(c == 'f' || c == 'e'){
+		k = alloc(tree);
+		tree[k].c = c;
+	}else if(c == 'p'){
+		k = alloc(tree);
+		tree[k].c = c;
+		
 	}
-	if(ca == cb && ca == 'e'){
-		pa += 1;
-		pb += 1;
-		return 0;
-	}
-	return result;
+}
+
+int calc(){
+
 }
 
 void solve(){
+	string a, b;
+	
 	getline(cin, a);
 	getline(cin, b);
-	pa = 0;
-	pb = 0;
-	printf("There are %d black pixels.\n", build(1024));
+	len = 0;p = 0;
+	build(tree1, a);
+	len = 0;p = 0;
+	build(tree2, b);
+	
+	printf("There are %d black pixels.\n", calc());
 }
 
 int main(void){
