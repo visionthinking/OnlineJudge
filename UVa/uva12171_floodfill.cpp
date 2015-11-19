7711f478b858dc358d8206f37d055de53f77243d
RainVision <rainvision2012@gmail.com>
2015-11-19 10:46:25 +0800

    Accepted. 使用了两种解法。

--- UVa/uva12171_floodfill.cpp
+++ UVa/uva12171_floodfill.cpp
@@ -1,36 +1,31 @@
 /* Uva12171, refer to book page 171. */
-/* Accepted. 72 ms. Just do flood fill with air area.
-   But, should be cautious with BFS searching space. 
-   If you put a `box` to BFS queue twice (or more), 
-   the queue may be overflowed.
-*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <algorithm>
 #include <map>
-
+#include <set>
 using namespace std;
 
+typedef long long ull;
 struct box {
 	int x0, y0, z0, x, y, z;	
 };
 
 const int MAX = 105;
-const int ID_MAX = 1005;
-const int MAX_DI = 1003;
 int xs[MAX], ys[MAX], zs[MAX];
-int idx[ID_MAX], idy[ID_MAX], idz[ID_MAX];
+map<int, int> idx, idy, idz;
 int a[MAX][MAX][MAX];
 struct box b[MAX];
 int n, xnum, ynum, znum;
-map<int, int> X, Y, Z;
+ull max_di;
 
 struct node {
-	int x, y, z, d;	
+	int x, y, z;	
 };
 struct node q[MAX*MAX*MAX];
-long long volume, surface;
+ull volume, surface;
 
 const int D[6][3] = {{1, 0, 0}, {-1, 0, 0},
                      {0, 1, 0}, {0, -1, 0},
@@ -46,9 +41,7 @@ void print(int * x, int len){
 
 void input(){
 	scanf("%d", &n);
-	xs[0] = ys[0] = zs[0] = 0;
-	xs[1] = ys[1] = zs[1] = MAX_DI;
-	
+	max_di = 1005;
 	for(int i=1;i<=n;i+=1){
 		scanf("%d%d%d%d%d%d", &b[i].x0, &b[i].y0, &b[i].z0, &b[i].x, &b[i].y, &b[i].z);
 		xs[i*2] = b[i].x0;
@@ -58,12 +51,14 @@ void input(){
 		zs[i*2] = b[i].z0;
 		zs[i*2+1] = b[i].z0 + b[i].z;
 	}
+	xs[0] = ys[0] = zs[0] = 0;
+	xs[1] = ys[1] = zs[1] = max_di;
 }
 
-int init(int * _xs, int * _idx){
+int init(int * _xs, map<int, int> & _idx){
 	sort(_xs, _xs+(n+1)*2);
 	int _xnum = unique(_xs, _xs+(n+1)*2) - _xs;
-	memset(_idx, -1, sizeof(int) * ID_MAX);
+	_idx.clear();
 	for(int i=0;i<_xnum;i+=1){
 		_idx[_xs[i]] = i;
 	}
@@ -74,17 +69,13 @@ bool inside(int x, int y, int z){
 	return (0 <= x && x < xnum-1 && 0 <= y && y < ynum-1 && 0 <= z && z < znum-1);
 }
 
-void flood_fill_air(){
-	int p, tx, ty, tz, qlen;
-	q[0].x = q[0].y = q[0].z = q[0].d = 0;
-	qlen = 1;
-	p = 0;
-	a[0][0][0] = -1;
+// marks/id: 0 for space, 2 for boxes, -1 for air-space, -2 for boxes-accessed.
+void flood_fill_air(int x0, int y0, int z0){
+	q[0].x = x0; q[0].y = y0; q[0].z = z0;
+	int p = 0, qlen = 1;
+	a[x0][y0][z0] = -1;
 	while(p < qlen){
-		const int & x = q[p].x;
-		const int & y = q[p].y;
-		const int & z = q[p].z;
-		volume += (xs[x+1]-xs[x])*(ys[y+1]-ys[y])*(zs[z+1]-zs[z]);
+		int x = q[p].x, y = q[p].y, z = q[p].z;
 		for(int i=0;i<6;i+=1){
 			int tx = x + D[i][0], ty = y + D[i][1], tz = z + D[i][2];
 			if(inside(tx, ty, tz) && a[tx][ty][tz] >= 0){
@@ -109,11 +100,34 @@ void flood_fill_air(){
 	}	
 }
 
+void flood_fill_boxes(int x0, int y0, int z0){
+	q[0].x = x0; q[0].y = y0; q[0].z = z0;
+	int p = 0, qlen = 1;
+	a[x0][y0][z0] = -2;
+	while(p < qlen){
+		int x = q[p].x, y = q[p].y, z = q[p].z;
+		volume += (xs[x+1]-xs[x])*(ys[y+1]-ys[y])*(zs[z+1]-zs[z]);
+		// printf("(%d %d %d) vol=%d\n", x, y, z, volume);
+		for(int i=0;i<6;i+=1){
+			int tx = x + D[i][0], ty = y + D[i][1], tz = z + D[i][2];
+			if(inside(tx, ty, tz) && a[tx][ty][tz] >= 0){
+				a[tx][ty][tz] = -2;
+				q[qlen].x = tx;
+				q[qlen].y = ty;
+				q[qlen].z = tz;
+				qlen += 1;
+			}
+		}
+		p += 1;
+	}	
+}
+
 void solve(){
 	xnum = init(xs, idx);
 	ynum = init(ys, idy);
 	znum = init(zs, idz);
 	memset(a, 0, sizeof(a));
+	int blk = 0;
 	for(int i=1;i<=n;i+=1){
 		int _x0 = idx[b[i].x0], _x1 = idx[b[i].x0 + b[i].x];
 		int _y0 = idy[b[i].y0], _y1 = idy[b[i].y0 + b[i].y];
@@ -122,15 +136,26 @@ void solve(){
 			for(int _y=_y0; _y < _y1; _y+=1){
 				for(int _z=_z0; _z < _z1; _z+=1){
 					a[_x][_y][_z] = 2;
+					// printf("(%d %d %d)\n", _x, _y, _z);
+					blk += 1;
 				}
 			}
 		}
 	}
-	// flood fill
 	volume = surface = 0;
-	flood_fill_air();
-	volume = MAX_DI*MAX_DI*MAX_DI - volume;
-	printf("%lld %lld\n", surface, volume);
+	// fill air-space
+	flood_fill_air(0, 0, 0);
+	// fill boxes
+	for(int _x=0; _x < xnum-1; _x+=1){
+		for(int _y=0; _y < ynum-1; _y+=1){
+			for(int _z=0; _z < znum-1; _z+=1){
+				if(a[_x][_y][_z] >= 0){
+					flood_fill_boxes(_x, _y, _z);
+				}
+			}
+		}	
+	}
+	printf("%llu %llu\n", surface, volume);
 }
 
 int main(void){
